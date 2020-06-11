5a7a7866d1a50dfab69466f2d34cbfda0be93229
George4888 <DanGeorge4888@gmail.com>
2020-06-11 18:21:56 +0300

    a few links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,26 +8,55 @@ export default function Portfolio() {
       <div className="projects">
         <div className="yearAPI">
           <p>Random Year API</p>
-          <img src="/photos/yearAPI.png" alt="year api" />
+          <a
+            href="http://randomyearapi.epizy.com/?i=1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/photos/yearAPI.png" alt="year api" />
+          </a>
           <div className="sources">
-            <p>web</p>
-            <p>github</p>
+            <a
+              href="https://github.com/George4888/random-year-api"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="/photos/GitHub.png" style={{ height: 35 }} alt="repo" />
+            </a>
           </div>
         </div>
         <div className="bootstrapSite">
           <p>Bootstrap 4</p>
-          <img src="/photos/bstrap4testing.png" alt="bootstrap site" />
+          <a
+            href="http://bootst4testing.epizy.com/?i=1#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/photos/bstrap4testing.png" alt="bootstrap site" />
+          </a>
           <div className="sources">
-            <p>web</p>
-            <p>github</p>
+            <a
+              href="https://github.com/George4888/Single-page-app"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="/photos/GitHub.png" style={{ height: 35 }} alt="repo" />
+            </a>
           </div>
         </div>
         <div className="groupProjct">
           <p>Team V Project</p>
-          <img src="/photos/teamV.png" alt="group project" />
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="/photos/teamV.png" alt="group project" />
+          </a>
           <div className="sources">
-            <p>web</p>
-            <p>github</p>
+            <a
+              href="https://github.com/Boo1122/IMDB"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="/photos/GitHub.png" style={{ height: 35 }} alt="repo" />
+            </a>
           </div>
         </div>
       </div>
