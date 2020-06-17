8860d04734bab8aa3372cea6397fa78d966312f4
George4888 <dangeorge4888@gmail.com>
2020-06-17 10:32:53 +0300

    target="_blank"

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -28,19 +28,22 @@ export default function Contact() {
       <div className="conImg">
         <motion.div className="line1" whileHover={{ scale: 1.1 }}>
           <p>Facebook</p>
-          <a href="https://www.facebook.com/dangeorge.caila.1">
+          <a href="https://www.facebook.com/dangeorge.caila.1" target="_blank">
             <img id="fb-contact" src="/photos/facebook.png" alt="facebook" />
           </a>
         </motion.div>
         <motion.div className="line2" whileHover={{ scale: 1.1 }}>
           <p>LinkedIn</p>
-          <a href="https://www.linkedin.com/in/dan-george-caila-660269191/">
+          <a
+            href="https://www.linkedin.com/in/dan-george-caila-660269191/"
+            target="_blank"
+          >
             <img id="link-contact" src="/photos/LinkedIn.png" alt="linkedin" />
           </a>
         </motion.div>
         <motion.div className="line3" whileHover={{ scale: 1.1 }}>
           <p>GitHub</p>
-          <a href="https://github.com/George4888">
+          <a href="https://github.com/George4888" target="_blank">
             <img id="git-contact" src="photos/Git3.png" alt="github" />
           </a>
         </motion.div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -88,12 +88,15 @@ export default function Portfolio() {
           whileHover={{ scale: 1.1 }}
         >
           <p>Random Year API</p>
-          <a href="http://randomyearapi.epizy.com">
+          <a href="http://randomyearapi.epizy.com" target="_blank">
             <img src="/photos/yearAPI.png" alt="year api" />
           </a>
           <div className="sources">
             <span>GitHub Repository</span>
-            <a href="https://github.com/George4888/random-year-api">
+            <a
+              href="https://github.com/George4888/random-year-api"
+              target="_blank"
+            >
               <img
                 src="/photos/GitHub.png"
                 style={{ height: 35, width: 35 }}
@@ -110,12 +113,15 @@ export default function Portfolio() {
           whileHover={{ scale: 1.1 }}
         >
           <p>Bootstrap 4</p>
-          <a href="http://bootst4testing.epizy.com">
+          <a href="http://bootst4testing.epizy.com" target="_blank">
             <img src="/photos/bstrap4testing.png" alt="bootstrap site" />
           </a>
           <div className="sources">
             <span>GitHub Repository</span>
-            <a href="https://github.com/George4888/Single-page-app">
+            <a
+              href="https://github.com/George4888/Single-page-app"
+              target="_blank"
+            >
               <img
                 src="/photos/GitHub.png"
                 style={{ height: 35, width: 35 }}
@@ -132,12 +138,12 @@ export default function Portfolio() {
           whileHover={{ scale: 1.1 }}
         >
           <p>Team V Project</p>
-          <a href="http://">
+          <a href="http://" target="_blank">
             <img src="/photos/teamV.png" alt="group project" />
           </a>
           <div className="sources">
             <span>GitHub Repository</span>
-            <a href="https://github.com/Boo1122/IMDB">
+            <a href="https://github.com/Boo1122/IMDB" target="_blank">
               <img
                 src="/photos/GitHub.png"
                 style={{ height: 35, width: 35 }}
