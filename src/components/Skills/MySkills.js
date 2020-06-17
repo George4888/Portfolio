d3afd958ea0cd5491dcb6756621e7007e4f692ef
George4888 <DanGeorge4888@gmail.com>
2020-06-17 15:00:52 +0300

    skill bar

--- src/components/Skills/MySkills.js
+++ src/components/Skills/MySkills.js
@@ -1,149 +1,94 @@
 import React from "react";
 import "./MySkills.css";
 import { motion, AnimatePresence } from "framer-motion";
+import Progress from "./Progress";
 
 export default function MySkills() {
-  const containerVariants = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: 0.5,
-        duration: 2,
-      },
-    },
-    exit: {
-      X: "-100vw",
-      transition: {
-        ease: "easeInOut",
-      },
-    },
-  };
-
-  const forH1 = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: 1,
-        duration: 1.5,
-      },
-    },
-  };
-
-  const childrenVariants = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: 2,
-        duration: 1.7,
-      },
-    },
-  };
-
-  const childrenVariants2 = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: 3,
-        duration: 1.7,
-      },
-    },
-  };
-
-  const childrenVariants3 = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: 4,
-        duration: 1.7,
-      },
-    },
-  };
-
   return (
-    <motion.div className="skillsPage">
-      <motion.h1 variants={forH1} initial="hidden" animate="visible">
-        I have experience working with
-      </motion.h1>
-
+    <div className="skillsPage">
+      <h1>I have experience working with</h1>
       <div className="logo">
-        <motion.div
-          className="section1"
-          variants={childrenVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <div className="html5">
-            <p>HTML 5</p>
-            <img src="/photos/HTML5.png" style={{ height: 70 }} alt="HTML5" />
+        <div className="list1">
+          <div className="section1">
+            <div className="html5">
+              <p>HTML 5</p>
+              <img src="/photos/HTML5.png" style={{ height: 35 }} alt="HTML5" />
+            </div>
+            <div className="prog">
+              <Progress done="90" />
+            </div>
           </div>
-          <div className="css3">
-            <p>CSS 3</p>
-            <img src="/photos/CSS3.png" style={{ height: 70 }} alt="CSS3" />
+          <div className="section2">
+            <div className="css3">
+              <p>CSS 3</p>
+              <img src="/photos/CSS3.png" style={{ height: 35 }} alt="CSS3" />
+            </div>
+            <div className="prog">
+              <Progress done="90" />
+            </div>
           </div>
-          <div className="js">
-            <p>JavaScript</p>
-            <img src="/photos/JS.png" style={{ height: 70 }} alt="JS" />
+          <div className="section3">
+            <div className="js">
+              <p>JavaScript</p>
+              <img src="/photos/JS.png" style={{ height: 35 }} alt="JS" />
+            </div>
+            <div className="prog">
+              <Progress done="80" />
+            </div>
           </div>
-        </motion.div>
+          <div className="section4">
+            <div className="re">
+              <p>React JS</p>
+              <img
+                src="/photos/ReactJS.png"
+                style={{ height: 35 }}
+                alt="ReactJS"
+              />
+            </div>
+            <div className="prog">
+              <Progress done="70" />
+            </div>
+          </div>
+        </div>
 
-        <motion.div
-          className="section2"
-          variants={childrenVariants2}
-          initial="hidden"
-          animate="visible"
-        >
-          <div className="re">
-            <p>React JS</p>
-            <img
-              src="/photos/ReactJS.png"
-              style={{ height: 70 }}
-              alt="ReactJS"
-            />
+        <div className="list2">
+          <div className="section5">
+            <div className="node">
+              <p>Node JS</p>
+              <img
+                src="/photos/NodeJS.png"
+                style={{ height: 35 }}
+                alt="Node JS"
+              />
+            </div>
+            <div className="prog">
+              <Progress done="70" />
+            </div>
           </div>
-          <div className="node">
-            <p>Node JS</p>
-            <img
-              src="/photos/NodeJS.png"
-              style={{ height: 70 }}
-              alt="Node JS"
-            />
+          <div className="section6">
+            <div className="web">
+              <p>Webpack</p>
+              <img
+                src="/photos/Webpack.png"
+                style={{ height: 35 }}
+                alt="Webpack"
+              />
+            </div>
+            <div className="prog">
+              <Progress done="70" />
+            </div>
           </div>
-        </motion.div>
-        <motion.div
-          className="section3"
-          variants={childrenVariants3}
-          initial="hidden"
-          animate="visible"
-        >
-          <div className="web">
-            <p>Webpack</p>
-            <img
-              src="/photos/Webpack.png"
-              style={{ height: 70 }}
-              alt="Webpack"
-            />
+          <div className="section7">
+            <div className="git">
+              <p>GitHub</p>
+              <img src="/photos/Git2.png" style={{ height: 35 }} alt="GitHub" />
+            </div>
+            <div className="prog">
+              <Progress done="85" />
+            </div>
           </div>
-          <div className="git">
-            <p>GitHub</p>
-            <img src="/photos/Git2.png" style={{ height: 70 }} alt="GitHub" />
-          </div>
-        </motion.div>
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 }
