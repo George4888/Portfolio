a78a1c1a89d7420e1e0319952da3644b7a23727e
George4888 <dangeorge4888@gmail.com>
2020-06-26 10:07:05 +0300

    useOnScreen function

---
+++ src/components/Skills/useOnScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function useOnScreen(options) {
+  const [ref, setRef] = React.useState(null);
+  const [visible, setVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, [ref, options]);
+
+  return [setRef, visible];
+}
